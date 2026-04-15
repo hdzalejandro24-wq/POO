c0350229e00470847a2b1740aff5f27484e497fd
hdzalejandro24-wq <hdzalejandro24@gmail.com>
2026-04-15 08:45:49 -0600

    Add files via upload

---
+++ EVA2_12_HERENCIA_INTERFACES/src/eva2_12_herencia_interfaces/EVA2_12_HERENCIA_INTERFACES.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_12_herencia_interfaces;
+
+/**
+ *
+ * @author carpi
+ */
+public class EVA2_12_HERENCIA_INTERFACES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+interface Prueba{
+void mostrarMensaje();
+}
+
+interface OtraPrueba extends Prueba{
+    void mostrarSaludo(String mensaje);
+}
+class Demostracion implement OtraPrueba{
+
+    @Override
+    public void mostrarSaludo() {
+        return super.
+    }
+    
+    
+}
