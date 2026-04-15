cb36207101f3e532d6fc179b3144a96c8a52f0b9
hdzalejandro24-wq <hdzalejandro24@gmail.com>
2026-04-15 08:42:08 -0600

    Add files via upload

---
+++ TestCovid/src/TestCovid.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carpi
+ */
+public class TestCovid{
+    private int edad;
+    private double peso;
+    private double estatura;
+    private boolean cronica;
+    public static void main(String[] args) {
+
+        public TestCovid(int edad, double peso, double estatura, boolean cronica) {
+            this.edad = edad;
+            this.peso = peso;
+            this.estatura = estatura;
+            this.cronica = cronica;
+        }
+        public double getPeso() {
+            return peso;
+        }
+
+        public double getEstatura() {
+            return estatura;
+        }
+
+        public boolean isCronica() {
+            return cronica;
+        }
+    
+    }
+public TestCovid(int edad. double peso. double estatura, boolean cronica);
+}
+private double calcularIMC(){
+
+}
+public String calcularPersonaRiesgo(){
+String resu =
+}
