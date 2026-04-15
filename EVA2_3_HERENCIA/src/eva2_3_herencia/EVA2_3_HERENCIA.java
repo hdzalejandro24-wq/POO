aaa685af3ebe2b038b95170b4b0746c35e4ab9f5
hdzalejandro24-wq <hdzalejandro24@gmail.com>
2026-04-15 08:43:39 -0600

    Add files via upload

--- EVA2_3_HERENCIA/src/eva2_3_herencia/EVA2_3_HERENCIA.java
+++ EVA2_3_HERENCIA/src/eva2_3_herencia/EVA2_3_HERENCIA.java
@@ -10,32 +10,55 @@ package eva2_3_herencia;
  */
 public class EVA2_3_HERENCIA {
 
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("--------Animal--------");
         Animal animal = new Animal();
         animal.comer();
+        System.out.println("--------Mamifero--------");
         Mamifero mamifero = new Mamifero();
+        mamifero.comer();
+        mamifero.tenerPelo();
+        System.out.println("--------Persona--------");
+        Persona persona = new Persona("Hola mundo!!");
+        persona.comer();
+        persona.tenerPelo();
+        persona.pensar();
+        // TODO code application logic here
     }
-    
-}
-class Animal{
-    public Animal(){
-   System.out.println("Clase animal");   
-}
-    public void comer (){
-     System.out.println("Animal: comer!!");
-    }
-class Mamifero{
-    public Mamifero(){
-   System.out.println("Clase mamifero");   
-}
-    
-}
     
 }
 
-class Persona{
+class Animal{// BASE, SUPERCLASE, PADRE
+    public Animal(){
+        System.out.println("Animal!!!");
+    }
     
+    public void comer(){
+        System.out.println("Comer!!!");
+    }
+}
+
+class Mamifero extends Animal{// DERIVADA, SUBCLASE, HIJO
+    public Mamifero(){
+        super();// CONSTRUCTOR DEFAULT DE LA SUPERCLASE
+        System.out.println("Mamifero!!!");
+    }
+    
+    public void tenerPelo(){
+        System.out.println("Tiene pelo!!!");
+    }
+}
+
+class Persona extends Mamifero{
+    public Persona(String mensaje){
+        super();// CONSTRUCTOR DEFAULT DE LA SUPERCLASE
+        System.out.println(mensaje);
+    }
+    public void pensar(){
+        System.out.println("Piensa!!!");
+    }
 }
