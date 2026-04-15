e67effef3c190655722c475bd92bad17e0d4f994
hdzalejandro24-wq <hdzalejandro24@gmail.com>
2026-04-15 08:46:20 -0600

    Add files via upload

---
+++ EVA_2_15_FINAL/src/eva_2_15_final/EVA_2_15_FINAL.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_15_final;
+
+/**
+ *
+ * @author carpi
+ */
+public class EVA_2_15_FINAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Empleado empleado = new empleado;
+    }
+    
+}
+
+
+class Persona{
+   private String nombre;
+   public Empleado;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+   
+}
+
+final class Empleado extends Persona
+        private String puesto;
+
+public empleado 
